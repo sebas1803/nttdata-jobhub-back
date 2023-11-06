620be1675c543bff3bb8a6b65e34c15348189628
Sebastian Santiago Alfaro Mendoza <sebas43243@hotmail.com>
2023-11-06 18:49:30 -0500

    Initial commit

---
+++ src/test/java/com/nttdata/jobhub/NttdataJobhubBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.jobhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttdataJobhubBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
